e6d7f5c23cc64322928adc62efaff3dc2d513606
Angela Keller <angela.keller@students.unibe.ch>
2017-10-24 22:44:01 +0200

    whatever, het d änderige no drufe irgendwie

--- src/main/java/hello/ProductOrder.java
+++ src/main/java/hello/ProductOrder.java
@@ -4,10 +4,7 @@ package hello;
  * Created by angelakeller on 16.10.17.
  * This class provides the product.html and saves the order details
 */
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity // This tells Hibernate to make a table out of this class
@@ -17,16 +14,9 @@ public class ProductOrder {
     private Integer id;
     private String product;
     private String amount;
-    private int clientId;
+    @ManyToOne
+    private Client client;
 
-
-    public int getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(int clientId) {
-        this.clientId = clientId;
-    }
 
     public Integer getId() {return id;}
     public String getProduct() { return product;}
@@ -36,4 +26,11 @@ public class ProductOrder {
     public void setProduct(String product) {this.product = product;}
     public void setAmount(String amount) {this.amount = amount;}
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
