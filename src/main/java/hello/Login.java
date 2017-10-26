7c637ebf647fbf25f244dd57ddfa7d536ee47ded
Angela Keller <angela.keller@students.unibe.ch>
2017-10-26 18:32:09 +0200

    worked for the login, created the needed java classes for it, incl. for the user registrations

---
+++ src/main/java/hello/Login.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by angelakeller on 26.10.17.
+ */
+/*public class Login {
+
+    private String username;
+    private String password;
+
+    public String getUsername() { return username; }
+    public String getPassword() { return  password; }
+
+    public void setUsername() { this.username = username; }
+    public void setPassword() { this.password = password; }
+}*/
---
+++ src/main/java/hello/UserDOA.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * Created by angelakeller on 26.10.17.
+ */
+/*
+public interface UserDOA {
+
+    void register(User user);
+
+    User validateUser(Login login);
+
+}*/
