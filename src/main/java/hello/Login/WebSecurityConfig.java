1e090a6669116a385c70398edc5b90558b331422
Jasmin Sert <jasmin.sert@students.unibe.ch>
2017-11-01 12:04:28 +0100

    still doesn't work, don't really know why..

---
+++ src/main/java/hello/Login/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package hello.Login;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/logistician/**").hasRole("LOGISTICIAN")
+                .antMatchers("/driver/**").hasRole("DRIVER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("logistician").password("password").roles("LOGISTICIAN");
+        auth
+                .inMemoryAuthentication()
+                .withUser("driver").password("password").roles("DRIVER");
+
+    }
+}
