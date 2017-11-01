8e953dffa7f8a5584e4bd79d23fa4e120431c936
Angela Keller <angela.keller@students.unibe.ch>
2017-11-01 01:37:09 +0100

    added packages and classes for driver and logistician

---
+++ src/main/java/hello/Driver/Driver.java
@@ -0,0 +1,22 @@
+package hello.Driver;
+
+/**
+ * Created by angelakeller on 01.11.17.
+ */
+public class Driver {
+
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String email;
+
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public int getPhoneNumber() {return phoneNumber;}
+    public String getEmail() {return email;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setPhoneNumber(int phoneNumber) {this.phoneNumber = phoneNumber;}
+    public void setEmail(String email) {this.email = email;}
+}
