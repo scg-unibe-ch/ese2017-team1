987f0edb17ef1c05e2e13b130c0f4aafa6a6f5ba
Corina Masanti <cmasanti@gmx.ch>
2017-11-06 21:17:42 +0100

    inserted data into client table (data.sql)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,37 @@ INSERT INTO `role` (`role`) VALUES
   ('ADMIN'),
   ('DRIVER'),
   ('LOGISTICIAN');
+
+INSERT INTO `client` (`id`, `city`, `company`, `email`, `land`, `name`, `street`) VALUES
+  (1, '2770-123 Pa?o de Arcos', '(977) 828-9760', 'cyclops.motors@example.com', 'Portugal', 'Cyclops Motors', 'Praceta Rosa 1'),
+  (2, 'Dunrobin, ON K0A 1T0', '(627) 137-1790', 'sail.navigations@example.com', 'Canada', 'Sail Navigations', '98 Porcupine Trail'),
+  (3, '45004 Toledo', '(567) 906-4052', 'rose.limited@example.com', 'Spain', 'Rose Limited', 'Ronda de el Olivar de los Pozos, 4'),
+  (4, '29196 M?laga', '(426) 707-3647', 'robinetworks@example.com', 'Spain', 'Robinetworks', 'Calle Generaci?n, 21-9'),
+  (5, 'Leixlip\nCo. Kildare', '(737) 688-0889', 'peachco@example.com', 'Ireland', 'Peachco', '363-371 Castletown'),
+  (6, 'Stoke-on-Trent\nST1 5HR\n', '(176) 380-3148', 'whiteoutwares@example.com', 'UK', 'Whiteoutwares', '151-153 Marsh St N'),
+  (7, '1090 Jette', '(356) 649-7639', 'griffindustries@example.com', 'Belgium', 'Griffindustries', 'Rue Alexandre Wouters 7-12'),
+  (8, '01-476 Warszawa', '(134) 469-9583', 'herbbooks@example.com', 'Poland', 'Herbbooks', 'Kaliskiego 25'),
+  (9, '38110 Braunschweig', '(676) 246-7637', 'shadowtronics@example.com', 'Germany', 'Shadowtronics', 'Im Steinkampe 14-16'),
+  (10, 'Moskva\n107140', '(555) 232-8189', 'nightwell@example.com', 'Russia', 'Nightwell', 'Komsomolskaya pl., 1'),
+  (11, 'Bucure?ti', '(716) 683-2661', 'melon.enterprises@example.com', 'Romania', 'Melon Enterprises', 'Pia?a Doroban?ilor 2'),
+  (12, 'Athina 104 35', '(822) 998-3532', 'granite.corporation@example.com', 'Greece', 'Granite Corporation', 'Iera Odos 36-44'),
+  (13, 'Vogo??a', '(587) 173-5823', 'eclipse.productions@example.com', 'Bosnia and Herzegovina', 'Eclipse Productions', 'Partizanskog odreda Zvijezda 2-22'),
+  (14, 'Il-Mosta', '(586) 499-4837', 'purplelimited@example.com', 'Malta', 'Purplelimited', '14 Triq Ir-Rifu?jati Tal-Gwerra'),
+  (15, '20016 Pero MI', '(114) 462-9810', 'omegacoustics@example.com', 'Italy', 'Omegacoustics', 'Via Bergamina, 14-18'),
+  (16, 'Budapest\n1196', '(979) 370-0150', 'jetechnologies@example.com', 'Hungary', 'Jetechnologies', '?rp?d u. 45-71'),
+  (17, '50825 K?ln', '(222) 393-9330', 'felinetworks@example.com', 'Germany', 'Felinetworks', 'Gr?ner Weg 3-1'),
+  (18, '6500 Landeck', '(727) 294-5007', 'nemoair@example.com', 'Austria', 'Nemoair', 'Perjenerweg 2'),
+  (19, '3600 Thun', '(177) 593-0085', 'peachstar@example.com', 'Switzerland', 'Peachstar', 'Burgerstrasse 13-15'),
+  (20, '1627 Vaulruz', '(530) 277-8373', 'hatchman@example.com', 'Switzerland', 'Hatchman', 'Vue-des-Alpes 19-1'),
+  (21, '6032 Emmen', '(912) 643-4135', 'sphinx.enterprises@example.com', 'Switzerland', 'Sphinx Enterprises', 'R?tistrasse 28'),
+  (22, '6330 Cham', '(494) 888-4751', 'core.security@example.com', 'Switzerland', 'Core Security', 'R?hrliberg 32'),
+  (23, '6932 Lugano', '(408) 472-1821', 'hurricane.systems@example.com', 'Switzerland', 'Hurricane Systems', 'Via Vergi? 8-18'),
+  (24, '3400 Burgdorf', '(743) 775-0073', 'signalimited@example.com', 'Switzerland', 'Signalimited', 'Meiefeldstrasse 34'),
+  (25, '1436 Chamblon', '(842) 959-3414', 'stardustechnologies@example.com', 'Switzerland', 'Stardustechnologies', 'Les Chandel?nes 2-4'),
+  (26, '1920 Martigny', '(540) 252-2091', 'cyclolutions@example.com', 'Switzerland', 'Cyclolutions', 'Chemin du Ch?teau 2-8'),
+  (27, '5032 Aarau', '(895) 953-6977', 'galerprises@example.com', 'Switzerland', 'Galerprises', 'Hauptstrasse 37'),
+  (28, '6340 Baar', '(708) 622-2003', 'pixelmaster@example.com', 'Switzerland', 'Pixelmaster', 'Neuhofstrasse 2'),
+  (29, '2802 Develier', '(681) 776-9988', 'shadetales@example.com', 'Switzerland', 'Shadetales', 'Chemin de la Combatte 3'),
+  (30, '7000 Chur', '(804) 820-2271', 'vortexbar@example.com', 'Switzerland', 'Vortexbar', 'Giacomettistrasse 114-116');
+
+
