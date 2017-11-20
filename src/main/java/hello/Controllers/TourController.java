fafec0d9bb7dc3fac270a2a22da06c74bc482d1e
Damaris Meier <meier.damaris@gmail.com>
2017-11-20 14:41:30 +0100

    some more work on Tour

--- src/main/java/hello/Controllers/TourController.java
+++ src/main/java/hello/Controllers/TourController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.ArrayList;
+
 
 @Controller
 public class TourController extends WebMvcConfigurerAdapter {
@@ -29,21 +31,37 @@ public class TourController extends WebMvcConfigurerAdapter {
     private ProductOrderRepository productOrderRepository;
 
 
+    @RequestMapping(value="/showTours")
+    public String listTours(@ModelAttribute("tour") Tour tour, Model model){
+        Iterable<Tour> allTours = this.tourRepository.findAll();
+        ArrayList<Tour> tours = new ArrayList<>();
+        for (Tour tour1 : allTours){
+            if(tour1.getDriver() != null && tour1.getVehicle()!= null && tour1.getTrailer()!= null){
+                tours.add(tour1);
+            }
+        }
+        model.addAttribute("tours", tours);
+        return "showTours";
+    }
+
     @RequestMapping(value="/newTour")
     public String list(@ModelAttribute("driver") Driver driver, Model model){
         Iterable<Driver> drivers = this.driverRepository.findAll();
         model.addAttribute("drivers", drivers);
+        Tour tour = new Tour();
+        this.tourRepository.save(tour);
+        model.addAttribute("tour", tour);
         return "newTour";
     }
 
 
-    @RequestMapping(value="/newTourTruck/{driverId}")
-    public String tourDriver(@PathVariable("driverId") Long driverId, Model model) {
+    @RequestMapping(value="/newTourTruck/{tourId}/{driverId}")
+    public String tourDriver(@PathVariable("tourId") Long tourId, @PathVariable("driverId") Long driverId, Model model) {
         Iterable<Vehicle> vehicles = this.vehicleRepository.findAll();
         model.addAttribute("vehicles", vehicles);
         Driver driver = this.driverRepository.findOne(driverId);
         model.addAttribute("driver", driver);
-        Tour tour = new Tour();
+        Tour tour = this.tourRepository.findOne(tourId);
         tour.setDriver(driver);
         this.tourRepository.save(tour);
         model.addAttribute("tour", tour);
@@ -65,34 +83,108 @@ public class TourController extends WebMvcConfigurerAdapter {
         return "newTourTrailer";
     }
 
+    @RequestMapping(value="/newTourProductOrders/{tourId}")
+    public String tourProducts(@PathVariable("tourId") Long tourId, Model model) {
+        Tour tour = this.tourRepository.findOne(tourId);
+        model.addAttribute("tour",tour);
+        Iterable<ProductOrder> productOrders = this.productOrderRepository.findAll();
+
+        ArrayList<ProductOrder> products = new ArrayList<>();
+        for(ProductOrder productOrder : productOrders){
+            if(productOrder.getTour() == null){
+                products.add(productOrder);
+            }
+        }
+        model.addAttribute("products", products);
+
+        ArrayList<ProductOrder> tourProducts = new ArrayList<>();
+        for (ProductOrder productOrder : productOrders){
+            if(productOrder.getTour() == tour){
+                tourProducts.add(productOrder);
+            }
+        }
+        model.addAttribute("tourProducts", tourProducts);
+        return "newTourProductOrders";
+    }
+
     @RequestMapping(value="/newTourProductOrders/{tourId}/{trailerId}")
     public String tourTrailer(@PathVariable("tourId") Long tourId, @PathVariable("trailerId") Long trailerId, Model model) {
-        Iterable<ProductOrder> products = this.productOrderRepository.findAll();
-        model.addAttribute("products", products);
         Tour tour = this.tourRepository.findOne(tourId);
         Trailer trailer = this.trailerRepository.findOne(trailerId);
         trailer.setFree(trailer.getFree() - 1);
         tour.setTrailer(trailer);
         this.trailerRepository.save(trailer);
+
         Integer palettes = tour.getVehicle().getPalettesAmount() + tour.getTrailer().getPalettesAmount();
         tour.setFreePalettes(palettes);
         this.tourRepository.save(tour);
         model.addAttribute("trailer", trailer);
         model.addAttribute("tour", tour);
+
+        Iterable<ProductOrder> productOrders = this.productOrderRepository.findAll();
+
+        ArrayList<ProductOrder> products = new ArrayList<>();
+        for(ProductOrder productOrder : productOrders){
+            if(productOrder.getTour() == null){
+                products.add(productOrder);
+            }
+        }
+        model.addAttribute("products", products);
+
+        ArrayList<ProductOrder> tourProducts = new ArrayList<>();
+        for (ProductOrder productOrder : productOrders){
+            if(productOrder.getTour() == tour){
+                tourProducts.add(productOrder);
+            }
+        }
+        model.addAttribute("tourProducts", tourProducts);
         return "newTourProductOrders";
     }
 
-    @RequestMapping(value="/newTourAddedProduct/{tourId}/{prodId}")
-    public String tourProduct(@PathVariable("tourId") Long tourId, @PathVariable("prodId") Long prodId, Model model) {
+    @RequestMapping(value="/newTourProductOrders/{tourId}/{prodId}/{add}")
+    public String tourProduct(@PathVariable("tourId") Long tourId, @PathVariable("prodId") Long prodId, @PathVariable ("add") Long add, Model model) {
         Tour tour = this.tourRepository.findOne(tourId);
         ProductOrder product = this.productOrderRepository.findOne(prodId);
-        product.setTour(tour);
-        tour.setFreePalettes(tour.getFreePalettes()-product.getProduct().getPalettes());
+        if(add==1){
+            if(product.getTour()==null){
+                Integer free = tour.getFreePalettes()-(product.getProduct().getPalettes()*Integer.parseInt(product.getAmount()));
+                if(free >= 0){
+                    product.setTour(tour);
+                    tour.setFreePalettes(free);
+                }
+            }
+        }
+        if(add==(-1)){
+            if(product.getTour()!=null){
+                if(product.getTour().getId()==tourId){
+                    product.setTour(null);
+                    tour.setFreePalettes(tour.getFreePalettes()+(product.getProduct().getPalettes()*Integer.parseInt(product.getAmount())));
+                }
+            }
+        }
         this.productOrderRepository.save(product);
         this.tourRepository.save(tour);
         model.addAttribute("product", product);
         model.addAttribute("tour", tour);
-        return "newTourAddedProduct";
+
+        Iterable<ProductOrder> productOrders = this.productOrderRepository.findAll();
+
+        ArrayList<ProductOrder> products = new ArrayList<>();
+        for(ProductOrder productOrder : productOrders){
+            if(productOrder.getTour() == null){
+                products.add(productOrder);
+            }
+        }
+        model.addAttribute("products", products);
+
+        ArrayList<ProductOrder> tourProducts = new ArrayList<>();
+        for (ProductOrder productOrder : productOrders){
+            if(productOrder.getTour() == tour){
+                tourProducts.add(productOrder);
+            }
+        }
+        model.addAttribute("tourProducts", tourProducts);
+        return "newTourProductOrders";
     }
 
 }
