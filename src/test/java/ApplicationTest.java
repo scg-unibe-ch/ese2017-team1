a440af49181091d3066b903758eb953445d9cd72
aurorra <cmasanti@gmx.ch>
2017-11-25 00:57:36 +0100

    trying some stuff out, not done yet.

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,5 +1,3 @@
-package hello;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.Client.Client;
+import hello.Controllers.ClientController;
+import hello.Controllers.LoginController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SmokeTest {
+
+    /**
+     * Tests whether application creates controllers
+     * Does not work yet, don't know why though...
+     */
+    /*@Autowired
+    private LoginController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }*/
+}
