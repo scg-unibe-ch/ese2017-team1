6f1e1aa472e8b9183270f2822a3394a3cc1c5694
Corina Masanti <cmasanti@gmx.ch>
2017-11-28 16:54:14 +0100

    tests dont fucking work

---
+++ src/test/java/RandomPortExampleTests.java
@@ -0,0 +1,34 @@
+import hello.Controllers.DriverController;
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.test.mock.mockito.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(DriverController.class)
+public class RandomPortExampleTests {
+
+    /*@Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private DriverController driverController;
+
+    @Test
+    public void testExample() throws Exception {
+        given(this.userVehicleService.getVehicleDetails("sboot"))
+                .willReturn(new VehicleDetails("Honda", "Civic"));
+        this.mvc.perform(get("/sboot/vehicle").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk()).andExpect(content().string("Honda Civic"));
+    }*/
+
+}
