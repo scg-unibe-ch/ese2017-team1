0cdc8b8469d974293bcfd935f2f7a3e33ca4e023
Corina Masanti <cmasanti@gmx.ch>
2017-12-06 14:19:24 +0100

    security

--- src/main/java/hello/Configuration/SecurityConfiguration.java
+++ src/main/java/hello/Configuration/SecurityConfiguration.java
@@ -95,7 +95,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/driver").hasAuthority("ROLE_DRIVER")
                 .antMatchers("/showToursDriver").hasAuthority("ROLE_DRIVER")
                 .antMatchers("/acceptedOrRejected").hasAuthority("ROLE_DRIVER")
-                .antMatchers("/driverTours").hasAuthority("ROLE_DRIVER")
+                .antMatchers("/driverTours/**").hasAuthority("ROLE_DRIVER")
                 .antMatchers("/driverProductOrder").hasAuthority("ROLE_DRIVER")
 
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN").anyRequest()
