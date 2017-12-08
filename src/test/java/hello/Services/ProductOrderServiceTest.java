9ae00af9976704d45625c60defc3cafa579d647c
Corina Masanti <cmasanti@gmx.ch>
2017-12-09 00:18:34 +0100

    too tired now

--- src/test/java/hello/Services/ProductOrderServiceTest.java
+++ src/test/java/hello/Services/ProductOrderServiceTest.java
@@ -1,11 +1,14 @@
 package hello.Services;
 
 import hello.Application;
+import hello.ProductOrders.ProductOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,6 +23,11 @@ public class ProductOrderServiceTest {
 
     @Test
     public void listAccProductOrders() throws Exception {
+
+        ArrayList<ProductOrder> products = new ArrayList<>();
+
+        ProductOrder productOrder = new ProductOrder();
+
 
 
 
