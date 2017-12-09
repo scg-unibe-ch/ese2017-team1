f64e4dbc82279893dd694c47a7a61c594fe30a73
Corina Masanti <cmasanti@gmx.ch>
2017-12-10 00:02:52 +0100

    working on trailerServiceTest, not done, but too tired

--- src/test/java/hello/Services/TrailerServiceTest.java
+++ src/test/java/hello/Services/TrailerServiceTest.java
@@ -1,13 +1,19 @@
 package hello.Services;
 
+import hello.Repositories.TrailerRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Created by Corina on 09.12.2017.
+ * Tests functionality of TrailerService class.
+ * We do not test the methods listAllTailers(), findTrailer() and save() since they only depend on the TrailerRepository
+ * to work and do not have a complex logic.
  */
 @RunWith(SpringRunner.class)
 public class TrailerServiceTest {
@@ -23,4 +29,23 @@ public class TrailerServiceTest {
     @Autowired
     private TrailerService trailerService;
 
+    @MockBean
+    private TrailerRepository trailerRepository;
+
+
+    @Before
+    public void setup(){
+
+    }
+
+
+    @Test
+    public void listTrailers(){
+
+    }
+
+    @Test
+    public void listUsedTrailers(){
+
+    }
 }
