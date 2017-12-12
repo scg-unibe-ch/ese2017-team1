57bb9c8dc87405b9be28d1927074b0b55f2d3cbe
Corina Masanti <cmasanti@gmx.ch>
2017-12-12 10:20:34 +0100

    ClientController, one test more

--- src/test/java/hello/Controllers/ClientRequestTest.java
+++ src/test/java/hello/Controllers/ClientRequestTest.java
@@ -1,25 +1,29 @@
 package hello.Controllers;
 
+import hello.Repositories.ClientRepository;
+import hello.Services.ClientService;
 import hello.Services.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(DriverController.class)
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ClientRequestTest {
 
     private MockMvc mockMvc;
@@ -51,10 +55,27 @@ public class ClientRequestTest {
     @MockBean
     private VehicleService vehicleService;
 
+    @MockBean
+    private ClientRepository clientRepository;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    ClientController clientController;
+
+
+    @TestConfiguration
+    static class ClientControllerTestContextConfiguration {
+        @Bean
+        public ClientController clientController() {
+            return new ClientController();
+        }
+    }
 
     @Before
     public void setup() {
+
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/view/");
         viewResolver.setSuffix(".html");
@@ -62,6 +83,8 @@ public class ClientRequestTest {
         mockMvc = MockMvcBuilders.standaloneSetup(new ClientController())
                 .setViewResolvers(viewResolver)
                 .build();
+
+        //mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
 
@@ -72,14 +95,41 @@ public class ClientRequestTest {
                 .andExpect(view().name("selectClient"));
     }
 
-    // does not work yet
+    @Test
+    public void clientForm() throws Exception {
+        this.mockMvc.perform(get("/clientForm"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("clientForm"));
+    }
+
     /*@Test
     public void getSearchClient() throws Exception {
+
+        ArrayList<Client> clients = new ArrayList<>();
+        Client client = new Client();
+        client.setCity("City");
+        client.setId((long) 1);
+        client.setLand("Land");
+        client.setStreet("Street");
+        client.setPhone("063 255 25 25");
+        client.setEmail("c@email.com");
+        client.setName("Name");
+        clients.add(client);
+
+        this.clientController.clientService = new ClientService();
+        this.clientController.clientService.clientRepository = clientRepository;
+
+        given(this.clientController.clientService.clientRepository.findAll())
+                .willReturn(clients);
+
+
+        given(this.clientController.clientService.listClients())
+                .willReturn(clients);
+
+
         this.mockMvc.perform(get("/searchClient"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("searchClient"));
     }*/
-
-
 }
 
